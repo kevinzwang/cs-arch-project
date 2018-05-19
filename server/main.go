f83273671621467bb1ec5743d76c583573029d27
Kevin Wamg <20215378+kevinzwang@users.noreply.github.com>
2018-05-19 15:39:31 -0700

    website dir

--- server/main.go
+++ server/main.go
@@ -1,53 +1,159 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
+	"./games"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
+var tokens = make(map[int64]bool)
 
 func main() {
 	r := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("assets/"))
+	fs := http.FileServer(http.Dir("website/assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	r.HandleFunc("/api/connectfour", connectFourHandler)
-	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/api/{game}", GameHandler)
+	r.HandleFunc("/view/{game}/{token}", ViewHandler)
+	r.HandleFunc("/new-token", TokenGenerateHandler)
+	r.HandleFunc("/list-tokens", ListTokenHandler)
+	r.HandleFunc("/docs/{game}", DocsHandler)
+	r.HandleFunc("/", HomeHandler)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	// appengine.Main()
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+// HomeHandler handles the home page for the site
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("website/templates/index.html"))
 	tmpl.Execute(w, nil)
 }
 
-func connectFourHandler(w http.ResponseWriter, r *http.Request) {
+// ViewHandler allows you to view the game
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Game: "+mux.Vars(r)["game"])
+	fmt.Fprintln(w, "Token: "+mux.Vars(r)["token"])
+}
+
+func DocsHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("website/templates/docs/" + mux.Vars(r)["game"] + ".html")
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+// GameHandler handles API calls for games
+func GameHandler(w http.ResponseWriter, r *http.Request) {
+	gameName := mux.Vars(r)["Name"]
+
+	var gameStruct games.Game
+	switch gameName {
+	case "connectfour":
+		gameStruct = &games.ConnectFour{}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade error:", err)
+		log.Print("upgrade error: ", err)
 		return
 	}
-
 	defer conn.Close()
 
-	ping := new(map[interface{}]interface{})
-	err = conn.ReadJSON(ping)
-	if err != nil {
-		log.Print("read json error:", err)
+	token, ok := authenticate(conn)
+	if !ok {
 		return
 	}
+	defer func() { tokens[token] = false }()
 
-	pingAsJSON, _ := json.Marshal(ping)
-	fmt.Println(pingAsJSON)
+	gameStruct.Execute(conn)
+}
+
+func authenticate(conn *websocket.Conn) (int64, bool) {
+	var msg map[interface{}]interface{}
+	err := conn.ReadJSON(&msg)
+	if err != nil {
+		log.Print("read json error: ", err)
+		conn.WriteJSON(map[string]interface{}{
+			"status": "failure",
+			"error":  "Couldn't read JSON",
+		})
+		return 0, false
+	}
+
+	token, ok := msg["token"]
+	if !ok {
+		conn.WriteJSON(map[string]interface{}{
+			"status": "failure",
+			"error":  "Couldn't get token from JSON",
+		})
+		return 0, false
+	}
+
+	fmtToken := int64(token.(float64))
+
+	playing, ok := tokens[fmtToken]
+
+	if !ok {
+		conn.WriteJSON(map[string]interface{}{
+			"status": "failure",
+			"error":  "No such token",
+		})
+		return 0, false
+	}
+
+	if playing {
+		conn.WriteJSON(map[string]interface{}{
+			"status": "failure",
+			"error":  "Already an instance playing",
+		})
+		return 0, false
+	}
+
+	dt := time.Now().Unix() - fmtToken
+	if dt > 172800 {
+		delete(tokens, fmtToken)
+		conn.WriteJSON(map[string]interface{}{
+			"status": "failure",
+			"error":  "Token has expired",
+		})
+		return 0, false
+	}
+
+	tokens[fmtToken] = true
+	conn.WriteJSON(map[string]interface{}{
+		"status": "connected",
+	})
+	return fmtToken, true
+}
+
+// TokenGenerateHandler generates tokens to use
+func TokenGenerateHandler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Unix()
+	_, err := fmt.Fprint(w, t)
+	if err != nil {
+		log.Print("http print error: ", err)
+		return
+	}
+	tokens[t] = false
+}
+
+// ListTokenHandler lists the tokens that are currently available
+func ListTokenHandler(w http.ResponseWriter, r *http.Request) {
+	for t := range tokens {
+		fmt.Fprintln(w, t)
+	}
 }
